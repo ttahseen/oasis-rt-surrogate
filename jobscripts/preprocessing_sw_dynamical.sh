546f315f953a80e9373367e44c21e5bdb153cd27
Tara Tahseen (Hypatia) <ucaptp0@ucl.ac.uk>
2024-02-09 15:06:54 +0000

    Fixed all bugs in preprocessing_sw_dynamical.py

--- jobscripts/preprocessing_sw_dynamical.sh
+++ jobscripts/preprocessing_sw_dynamical.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-#requesting one node
 #SBATCH -N1
-#requesting 1 cores
 #SBATCH -n1
 #SBATCH --mail-user=ucaptp0@ucl.ac.uk
 #SBATCH --mail-type=ALL
-#SBATCH --mem=350G 
 
-source /share/apps/anaconda3-2021.05/etc/profile.d/conda.sh #for bash 
+source /share/apps/anaconda/3-2022.05/etc/profile.d/conda.sh #for bash 
 conda activate
 conda activate ml-gcm-env
 python /home/ucaptp0/oasis-rt-surrogate/model/rnn_sw/preprocessing_sw_dynamical.py
