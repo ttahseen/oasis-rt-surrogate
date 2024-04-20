30f70a4abd630441b9463cd451abbeba3e2aee2f
Tara Tahseen (Hypatia) <ucaptp0@ucl.ac.uk>
2024-04-20 21:27:22 +0100

    Changed size of indent from 2 to 4 spaces

--- inference/inference.cpp
+++ inference/inference.cpp
@@ -9,108 +9,109 @@
 
 const OrtApi* g_ort = OrtGetApiBase()->GetApi(ORT_API_VERSION);
 
-void CheckStatus(OrtStatus* status)
-{
-  if (status != NULL) {
-    const char* msg = g_ort->GetErrorMessage(status);
-    fprintf(stderr, "%s\n", msg);
-    g_ort->ReleaseStatus(status);
-    exit(1);
-  }
+void CheckStatus(OrtStatus* status) {
+    if (status != NULL) {
+        const char* msg = g_ort->GetErrorMessage(status);
+        fprintf(stderr, "%s\n", msg);
+        g_ort->ReleaseStatus(status);
+        exit(1);
+    }
 }
 
 #define ORT_ABORT_ON_ERROR(expr)                             \
-  do {                                                       \
-    OrtStatus* onnx_status = (expr);                         \
-    if (onnx_status != NULL) {                               \
-      const char* msg = g_ort->GetErrorMessage(onnx_status); \
-      fprintf(stderr, "%s\n", msg);                          \
-      g_ort->ReleaseStatus(onnx_status);                     \
-      abort();                                               \
-    }                                                        \
-  } while (0);
+    do {                                                       \
+        OrtStatus* onnx_status = (expr);                         \
+        if (onnx_status != NULL) {                               \
+            const char* msg = g_ort->GetErrorMessage(onnx_status); \
+            fprintf(stderr, "%s\n", msg);                          \
+            g_ort->ReleaseStatus(onnx_status);                     \
+            abort();                                               \
+        }                                                        \
+    } while (0);
 
 int main() {
-
-  OrtEnv* env = nullptr;
-  OrtSession* session = nullptr;
-
-  CheckStatus(g_ort->CreateEnv(ORT_LOGGING_LEVEL_VERBOSE, "test", &env));
-
-  OrtSessionOptions* session_options = nullptr;
-  CheckStatus(g_ort->CreateSessionOptions(&session_options));
-
-  // set numthreads
-  ORT_ABORT_ON_ERROR(g_ort->SetIntraOpNumThreads(session_options, 1));
-  ORT_ABORT_ON_ERROR(g_ort->SetSessionGraphOptimizationLevel(session_options, ORT_ENABLE_BASIC));
-
-  const char* model_path = "/home/ucaptp0/oasis-rt-surrogate/trained_models/onnx/rnn_sw/dynamical/649968_combined_inputs.onnx";
-  printf("Using Onnxruntime C API\n");
-
-  CheckStatus(g_ort->CreateSession(env, model_path, session_options, &session));
-
-
-  size_t input_tensor_size = 54 * 3;
-  // float* input_tensor_values = new float[54 * 3]{0.5};
-
-  H5Easy::File file("/home/ucaptp0/oasis-rt-surrogate/inference/data/sw_single_inputs.h5", H5Easy::File::ReadOnly);
-  // read from dataset
-  std::vector<std::vector<double>> input_vector = H5Easy::load<std::vector<std::vector<double>>>(file, "/inputs");
-
-  // float* input_tensor_values = new float[input_vector.size()];
-  float* input_tensor_values = new float[54 * 3];
-
-  // Copy elements from the 2D vector into the float* array
-  size_t index = 0;
-  for (const std::vector<double>& row : input_vector) {
-      for (const double& element : row) {
-          input_tensor_values[index++] = static_cast<float>(element);
-      }
-  }
-
-  const char *input_names[] = {"input_8"};
-  const char *output_names[] = { "dense_output" };
-  const int64_t input_shape[] = {1, 54, 3};
-  const size_t model_input_len = input_tensor_size * sizeof(float);
-  const size_t input_shape_len = sizeof(input_shape) / sizeof(input_shape[0]);
-  printf("The model_input length %zu\n", model_input_len);
-
-  OrtMemoryInfo* memory_info = nullptr;
-  CheckStatus(g_ort->CreateCpuMemoryInfo(OrtArenaAllocator, OrtMemTypeDefault, &memory_info));
-  OrtValue *input_tensor = nullptr;
-  CheckStatus(g_ort->CreateTensorWithDataAsOrtValue(memory_info,
-                                                    input_tensor_values,
-                                                    model_input_len,
-                                                    input_shape,
-                                                    input_shape_len,
-                                                    ONNX_TENSOR_ELEMENT_DATA_TYPE_FLOAT,
-                                                    &input_tensor));
-
-  assert(input_tensor != NULL);
-  int is_tensor;
-  CheckStatus(g_ort->IsTensor(input_tensor, &is_tensor));
-  assert(is_tensor);
-
-  OrtValue* output_tensor = nullptr;
-
-  printf("start to run onnxruntime\n");
-  ORT_ABORT_ON_ERROR(g_ort->Run(session,
-                         nullptr,
-                         input_names,
-                         (const OrtValue* const*)&input_tensor,
-                         1,
-                         output_names,
-                         1,
-                         &output_tensor));
-
-  assert(output_tensor != NULL);
-  printf("finish!!!");
-
-  g_ort->ReleaseValue(output_tensor);
-  g_ort->ReleaseValue(input_tensor);
-  g_ort->ReleaseSession(session);
-  g_ort->ReleaseSessionOptions(session_options);
-  g_ort->ReleaseEnv(env);
-
-  return 0;
+  
+    OrtEnv* env = nullptr;
+    OrtSession* session = nullptr;
+    
+    CheckStatus(g_ort->CreateEnv(ORT_LOGGING_LEVEL_VERBOSE, "test", &env));
+    
+    OrtSessionOptions* session_options = nullptr;
+    CheckStatus(g_ort->CreateSessionOptions(&session_options));
+    
+    // set numthreads
+    ORT_ABORT_ON_ERROR(g_ort->SetIntraOpNumThreads(session_options, 1));
+    ORT_ABORT_ON_ERROR(g_ort->SetSessionGraphOptimizationLevel(session_options, ORT_ENABLE_BASIC));
+    
+    const char* model_path = "/home/ucaptp0/oasis-rt-surrogate/trained_models/onnx/rnn_sw/dynamical/649968_combined_inputs.onnx";
+    printf("Using Onnxruntime C API\n");
+    
+    CheckStatus(g_ort->CreateSession(env, model_path, session_options, &session));
+    
+    
+    size_t input_tensor_size = 54 * 3;
+    // float* input_tensor_values = new float[54 * 3]{0.5};
+    
+    H5Easy::File file("/home/ucaptp0/oasis-rt-surrogate/inference/data/sw_single_inputs.h5", H5Easy::File::ReadOnly);
+    // read from dataset
+    std::vector<std::vector<double>> input_vector = H5Easy::load<std::vector<std::vector<double>>>(file, "/inputs");
+    
+    // float* input_tensor_values = new float[input_vector.size()];
+    float* input_tensor_values = new float[54 * 3];
+    
+    // Copy elements from the 2D vector into the float* array
+    size_t index = 0;
+    for (const std::vector<double>& row : input_vector) {
+        for (const double& element : row) {
+            input_tensor_values[index++] = static_cast<float>(element);
+        }
+    }
+    
+    const char *input_names[] = {"input_8"};
+    const char *output_names[] = { "dense_output" };
+    const int64_t input_shape[] = {1, 54, 3};
+    const size_t model_input_len = input_tensor_size * sizeof(float);
+    const size_t input_shape_len = sizeof(input_shape) / sizeof(input_shape[0]);
+    printf("The model_input length %zu\n", model_input_len);
+    
+    OrtMemoryInfo* memory_info = nullptr;
+    CheckStatus(g_ort->CreateCpuMemoryInfo(OrtArenaAllocator, OrtMemTypeDefault, &memory_info));
+    OrtValue *input_tensor = nullptr;
+    CheckStatus(g_ort->CreateTensorWithDataAsOrtValue(memory_info,
+                                                      input_tensor_values,
+                                                      model_input_len,
+                                                      input_shape,
+                                                      input_shape_len,
+                                                      ONNX_TENSOR_ELEMENT_DATA_TYPE_FLOAT,
+                                                      &input_tensor));
+    
+    assert(input_tensor != NULL);
+    int is_tensor;
+    CheckStatus(g_ort->IsTensor(input_tensor, &is_tensor));
+    assert(is_tensor);
+    
+    OrtValue* output_tensor = nullptr;
+    
+    printf("start to run onnxruntime\n");
+    ORT_ABORT_ON_ERROR(g_ort->Run(
+        session,
+        nullptr,
+        input_names,
+        (const OrtValue* const*)&input_tensor,
+        1,
+        output_names,
+        1,
+        &output_tensor
+    ));
+    
+    assert(output_tensor != NULL);
+    printf("finish!!!");
+    
+    g_ort->ReleaseValue(output_tensor);
+    g_ort->ReleaseValue(input_tensor);
+    g_ort->ReleaseSession(session);
+    g_ort->ReleaseSessionOptions(session_options);
+    g_ort->ReleaseEnv(env);
+    
+    return 0;
 }
